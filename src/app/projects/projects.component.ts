84a9b4463d18fe8c91e82551c68602ca165aa364
Jason <jbugallo@uci.edu>
2019-10-20 18:53:10 -0700

    redid about page, navbar, transformed from multi-page to single page, added resume, added better responsive design

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faAngular,
+  faCss3,
+  faFacebook,
+  faGit,
+  faGithub,
+  faHtml5, faJava,
+  faLinkedinIn,
+  faNodeJs,
+  faPython, faReact
+} from '@fortawesome/free-brands-svg-icons';
+import {faCode, faDatabase} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
@@ -6,7 +18,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  icons = {
+    linkedIn: faLinkedinIn,
+    github: faGithub,
+    facebook: faFacebook,
+    python: faPython,
+    javascript: faNodeJs,
+    database: faDatabase,
+    git: faGit,
+    html5: faHtml5,
+    css3: faCss3,
+    angular: faAngular,
+    react: faReact,
+    java: faJava,
+    code: faCode
+  };
 
+  hovers = {
+    fabflix: false
+  };
   constructor() { }
 
   ngOnInit() {
